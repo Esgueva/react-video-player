4b526567e392f10145dd99547c4c57052033efe8
Esgueva <esgueva@gmail.com>
2020-05-14 23:34:30 +0200

    feature/render-from-server

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,6 @@
+//IGNORE STYLES FROM SERVER
+require("ignore-styles");
+
 require("@babel/register")({
   presets: ["@babel/preset-env", "@babel/preset-react"],
 });
--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,17 @@ import express from "express";
 import dotenv from "dotenv";
 import webpack from "webpack";
 
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { StaticRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { renderRoutes } from "react-router-config";
+
+import reducer from "../frontend/reducers";
+import serverRoutes from "../frontend/routes/serverRoutes";
+import initialState from "../frontend/initialState";
+
 //ENV
 dotenv.config();
 const { ENV, PORT } = process.env;
@@ -17,28 +28,44 @@ if (ENV === "development") {
   const webpackHotMiddleware = require("webpack-hot-middleware");
   const compiler = webpack(webpackConfig);
   const serverConfig = { port: PORT, hot: true };
-
   app.use(webpackDevMiddleware(compiler, serverConfig));
   app.use(webpackHotMiddleware(compiler));
 }
 
-//ROUTER
-app.get("*", (req, res) => {
-  res.send(`
+// RESPONSE
+const setResponse = (html) => {
+  return `
     <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Video Player</title>
-        <link href="assets/app.css" type="text/css" rel="stylesheet">
-      </head>
-      <body>
-        <div id="app"></div>
-        <script src="assets/app.js" type="text/javascript"> </script> 
-      </body>
-    </html>
-  `);
-});
+      <html>
+        <head>
+          <title>Video Player</title>
+          <link href="assets/app.css" type="text/css" rel="stylesheet">
+        </head>
+        <body>
+          <div id="app">${html}</div>
+          <script src="assets/app.js" type="text/javascript"> </script> 
+        </body>
+      </html>
+  `;
+};
 
+//RENDER
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initialState);
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} contest={{}}>
+        {renderRoutes(serverRoutes)}
+      </StaticRouter>
+    </Provider>
+  );
+  res.send(setResponse(html));
+};
+
+//ROUTER
+app.get("*", renderApp);
+
+//SERVER
 app.listen(PORT, (err) => {
   if (err) console.log(err);
   else console.log(`Server ${ENV} running on port ${PORT}`);

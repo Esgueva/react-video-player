210cbd8a49015c87468ff345d6a772f85f17325a
Esgueva <esgueva@gmail.com>
2020-05-15 14:09:57 +0200

    optimization/split-vendor-js

--- src/server/server.js
+++ src/server/server.js
@@ -48,6 +48,7 @@ if (ENV === "development") {
 const setResponse = (html, preloadedState, manifest) => {
   const mainStyles = manifest ? manifest["main.css"] : "assets/app.css";
   const mainBuild = manifest ? manifest["main.js"] : "assets/app.js";
+  const vendorBuild = manifest ? manifest["vendors.js"] : "assets/vendor.js";
 
   return `
     <!DOCTYPE html>
@@ -63,7 +64,8 @@ const setResponse = (html, preloadedState, manifest) => {
               preloadedState
             ).replace(/</g, "\\u003c")}
           </script>
-          <script src="${mainBuild}" type="text/javascript"> </script> 
+          <script src="${mainBuild}" type="text/javascript"> </script>
+          <script src="${vendorBuild}" type="text/javascript"> </script>  
         </body>
       </html>
   `;
